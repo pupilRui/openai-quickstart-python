3647907fd081a909b1d96c85443009f7beee53e0
pupilRui <142526194+pupilRui@users.noreply.github.com>
2023-09-27 19:18:20 -0700

    python-web-based-solution

--- app.py
+++ app.py
@@ -1,35 +1,38 @@
 import os
 
 import openai
+
+from backend import cmd_based
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+
+with open(".env") as env:
+    for line in env:
+        key, value = line.strip().split("=")
+        os.environ[key] = value
+
+#openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.environ.get("API_KEY")
+openai.organization = os.environ.get("ORG_ID")
 
 
 @app.route("/", methods=("GET", "POST"))
 def index():
+    question = ''
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
+        question = request.form["question"]
+        return redirect(url_for("index", result=cmd_based.answer_question(question=question), question=question))
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+        # return redirect(url_for("index", result=cmd_based.answer_question(question=question)))
+
+    result = request.args.get("result") or ""
+    question = request.args.get("question") or "" 
+    return render_template("customer.html", result=result, question=question)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+def generate_prompt(question):
+    return """Help answer this question:
+question: {}""".format(
+        question.capitalize()
     )
